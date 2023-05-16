25e0ac41339b42d6f97ebda2829b62ae8f636307
hwawon <corn1128@naver.com>
2023-05-16 13:17:22 +0900

    update naver login

--- src/components/snsLogin/NaverLogin.jsx
+++ src/components/snsLogin/NaverLogin.jsx
@@ -1,32 +1,31 @@
 import { useEffect, useRef } from "react"
 import imgNaver from "assets/images/login-naver.png"
+import { useLocation } from "react-router-dom"
 
 const NaverLogin = () => {
+  const location = useLocation()
   const naverRef = useRef()
   const { naver } = window
-  const { NAVER_CLIENT_ID, NAVER_CALLBACK_URL } = process.env
+  const { REACT_APP_NAVER_CLIENT_ID, REACT_APP_NAVER_CALLBACK_URL } = process.env
 
   const initializeNaverLogin = () => {
     const naverLogin = new naver.LoginWithNaverId({
-      clientId: NAVER_CLIENT_ID,
-      callbackUrl: NAVER_CALLBACK_URL,
+      clientId: REACT_APP_NAVER_CLIENT_ID,
+      callbackUrl: REACT_APP_NAVER_CALLBACK_URL,
       isPopup: false,
       loginButton: { color: "green", type: 3, height: 58 },
       callbackHandle: true,
     })
     naverLogin.init()
   }
-  const getToken = () => {
-    const token = window.location.href.split("=")[1].split("&")[0]
-  }
-  const userAccessToken = () => {
-    // eslint-disable-next-line no-unused-expressions
-    window.location.href.includes("access_token") && getToken()
+  const getNaverToken = () => {
+    if (!location.hash) return
+    const token = location.hash.split("=")[1].split("&")[0]
   }
 
   useEffect(() => {
     initializeNaverLogin()
-    userAccessToken()
+    getNaverToken()
   }, [])
   const handleNaverLogin = () => {
     naverRef.current.children[0].click()
