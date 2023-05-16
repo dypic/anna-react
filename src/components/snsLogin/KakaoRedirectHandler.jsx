5edb1278ebfed1d8d29582e5bd7f0f61da93d843
hwawon <corn1128@naver.com>
2023-05-16 17:45:28 +0900

    카카오 유저정보 가져오기

--- src/components/snsLogin/KakaoRedirectHandler.jsx
+++ src/components/snsLogin/KakaoRedirectHandler.jsx
@@ -9,10 +9,11 @@ const KakaoRedirectHandler = () => {
     const code = params.get("code") // 인가코드 받는 부분
     const grant_type = "authorization_code"
     const client_id = process.env.REACT_APP_KAKAO_CLIENT_SECRET
+    const readurect_uri = process.env.REACT_APP_KAKAO_REDIRECT_URL
 
     axios
       .post(
-        `https://kauth.kakao.com/oauth/token?grant_type=${grant_type}&client_id=${client_id}&redirect_uri=http://localhost:3000/oauth/callback/kakao&code=${code}`,
+        `https://kauth.kakao.com/oauth/token?grant_type=${grant_type}&client_id=${client_id}&redirect_uri=${readurect_uri}&code=${code}`,
         {
           headers: {
             "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
@@ -21,10 +22,31 @@ const KakaoRedirectHandler = () => {
       )
       // .then((res) => res.json())
       .then((res) => {
-        localStorage.setItem("kakao-token", res.data.access_token)
-        navigate("/")
-        // res에 포함된 토큰 받아서 원하는 로직을 하면된다.
-        // 이후 백엔드 작업이 필요함
+        console.log(res)
+        const { data } = res
+        const { access_token } = data
+        if (access_token) {
+          console.log(`Bearer ${access_token}`)
+          axios
+            .post(
+              "https://kapi.kakao.com/v2/user/me",
+              {},
+              {
+                headers: {
+                  Authorization: `Bearer ${access_token}`,
+                  "Content-Type": "application/x-www-form-urlencoded",
+                },
+              },
+            )
+            .then((res) => {
+              console.log("데이터 성공:")
+              console.log(res)
+            })
+        } else {
+          console.log("access_token 없음!")
+        }
+        // res에 포함된 토큰 받아서 원하는 로직을 하면됩니다..
+        // 이후 백엔드 작업이 필요합니다.
       })
   }, [])
 
